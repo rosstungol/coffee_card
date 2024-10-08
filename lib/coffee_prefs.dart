2fbd17d94e66266927fdca66c38e1c25829bdfa2
Ross Jonathan Tungol <rossjstungol@gmail.com>
2024-10-08 13:39:19 +0800

    Adds FilledButton to CoffeePrefs widget

--- lib/coffee_prefs.dart
+++ lib/coffee_prefs.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class CoffeePrefs extends StatelessWidget {
   const CoffeePrefs({super.key});
 
+  void increaseStrength() {
+    print("inc strength by 1");
+  }
+
+  void increaseSugar() {
+    print("inc sugar by 1");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -18,7 +26,14 @@ class CoffeePrefs extends StatelessWidget {
               colorBlendMode: BlendMode.multiply,
             ),
             const Expanded(child: SizedBox()),
-            const Text("+"),
+            FilledButton(
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.brown,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: increaseStrength,
+              child: const Text('+'),
+            )
           ],
         ),
         Row(
@@ -32,7 +47,14 @@ class CoffeePrefs extends StatelessWidget {
               colorBlendMode: BlendMode.multiply,
             ),
             const Expanded(child: SizedBox()),
-            const Text("+"),
+            FilledButton(
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.brown,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: increaseSugar,
+              child: const Text('+'),
+            )
           ],
         ),
       ],
