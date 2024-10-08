6cc4832f32b4912fd9f6d38c6c248f11a1357f7b
Ross Jonathan Tungol <rossjstungol@gmail.com>
2024-10-08 11:39:35 +0800

    Adds Column widget to Home widget

--- lib/home.dart
+++ lib/home.dart
@@ -7,11 +7,28 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Coffee Id'),
+        title: const Text(
+          'My Coffee Id',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
         backgroundColor: Colors.brown[700],
         centerTitle: true,
       ),
-      body: const Text('Home'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.brown[200],
+            padding: const EdgeInsets.all(20),
+            child: const Text('How I like my coffee...'),
+          ),
+          Container(
+            color: Colors.brown[100],
+            padding: const EdgeInsets.all(20),
+            child: const Text('Coffee preferences'),
+          ),
+        ],
+      ),
     );
   }
 }
